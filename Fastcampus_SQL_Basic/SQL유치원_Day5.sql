1a9e68ac56cc3fe06b6c6b88c26af37dfe64e4dc
SeungHyuk Go <goshdding@gmail.com>
2022-03-15 00:45:43 +0900

    feat: update day5

--- Fastcampus_SQL_Basic/SQL유치원_Day5.sql
+++ Fastcampus_SQL_Basic/SQL유치원_Day5.sql
@@ -45,3 +45,49 @@ select name, attack,
 	   row_number() over(order by attack desc) as rank_row_number
 from mypokemon
 ;
+
+
+/*
+실습
+*/
+DROP DATABASE IF EXISTS pokemon;
+CREATE DATABASE pokemon;
+USE pokemon;
+CREATE TABLE mypokemon (
+number INT,
+name VARCHAR(20),
+type VARCHAR(10),
+attack INT,
+defense INT,
+capture_date DATE
+);
+INSERT INTO mypokemon (number, name, type, attack, defense, capture_date)
+VALUES (10, 'caterpie', 'bug', 30, 35, '2019-10-14'),
+(25, 'pikachu', 'electric', 55, 40, '2018-11-04'),
+(26, 'raichu', 'electric', 90, 55, '2019-05-28'),
+(125, 'electabuzz', 'electric', 83, 57, '2020-12-29'),
+(133, 'eevee', 'normal', 55, 50, '2021-10-03'),
+(137, 'porygon', 'normal', 60, 70, '2021-01-16'),
+(152, 'chikoirita', 'grass', 49, 65, '2020-03-05'),
+(153, 'bayleef', 'grass', 62, 80, '2022-01-01');
+
+-- Mission 1
+use pokemon;
+select name, length(name)
+from mypokemon
+order by 2
+;
+
+-- 2
+select name, 
+	   rank() over(order by defense desc) as defense_rank
+from mypokemon
+;
+
+-- 3
+select * from mypokemon;
+select name, datediff(capture_date, '2022-02-14') as days
+from mypokemon
+;
+
+-- 4
